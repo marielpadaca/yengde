bf49568e7febb55b7438cb3fa30222a40ddee149
Mariel Padaca <Mariel.Padaca@cri-dc.litfirepublishing.com>
2019-05-14 13:36:26 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mariel's Page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="body.css">
+</head>
+
+<body>
+<header>
+
+
+<nav class="navbar" >
+	<div class="container">
+		<!--
+		<a class="navbar-brand" href="#">
+          <img src="logo2.jpg" alt="" width="80%"; height:20px; >
+        </a> 
+    -->
+  <ul >
+ 	<div class="navcontainer">
+ 	
+		<li><a href="#">CONTACT</a></li>
+		<li><a href="#">PROJECTS</a></li>
+		<li><a  href="#">HOME</a></li>
+</ul>
+	</div>
+	</div>
+</nav> 
+</header>
+
+<main>
+<div class="container">
+	<div class="bodypar">
+		<div class="bodypar2">
+			<h1>Let's make something great together!</h1><br/>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+		</div>
+	</div>
+</div>
+
+<div id="link">
+	<div class="container">
+		<div class="link1">
+			<a href="#">My first link <span class="glyphicon glyphicon-chevron-right"></span></a>
+		</div>
+
+			<div class="link2">
+				<a href="#">My second link <span class="glyphicon glyphicon-chevron-right"></span></a>
+			</div>
+	</div>
+</div>
+
+
+
+
+</main>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
